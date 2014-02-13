4a9782b361ab996ca61a218d0fbf2238c702cffa
Simon <simon@bits.(none)>
2014-02-13 11:49:41 +0100

    Event Emitter with simple test case

---
+++ briee/emitter_test.go
@@ -0,0 +1,73 @@
+package briee
+
+import (
+	"sync"
+	"testing"
+	//"log"
+)
+
+type A struct {
+	Int int
+	Str string
+}
+
+type B struct {
+	Float float64
+	Int int
+}
+
+func TestEmitter(t *testing.T) {
+	var ee *LocalEventEmitter
+	ee = NewEventEmitter()
+	go ee.Run()
+
+	PublA := ee.Publish("A", A{}).(chan<- A)
+	PublA2 := ee.Publish("A", A{}).(chan<- A)
+	SubsB1 := ee.Subscribe("B", B{}).(<-chan B)
+	SubsA1 := ee.Subscribe("A", A{}).(<-chan A)
+	PublB := ee.Publish("B", B{}).(chan<- B)
+	SubsA2 := ee.Subscribe("A", A{}).(<-chan A)
+
+	Adata := A{42, "A data"}
+	Bdata := B{13.37, 7}
+
+	var recvA1, recvA2 A
+	var recvB1 B
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func(){
+		recvA1 = <-SubsA1
+		recvA2 = <-SubsA2
+		recvA1 = <-SubsA1
+		recvA2 = <-SubsA2
+		recvB1 = <-SubsB1
+		wg.Done()
+	}()
+
+	go func(){
+		PublA <- Adata
+		PublA2 <- Adata
+		PublB <- Bdata
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	if Adata != recvA1 {
+		t.Errorf("Got data %v, want %v", recvA1, Adata)
+	}
+
+	if Adata != recvA2 {
+		t.Errorf("Got data %v, want %v", recvA2, Adata)
+	}
+
+	if Bdata != recvB1 {
+		t.Errorf("Got data %v, want %v", recvB1, Bdata)
+	}
+	//fmt.Printf("Got data %v, want %v\n", recvA1, Adata)
+	//fmt.Printf("Got data %v, want %v\n", recvA2, Adata)
+	//fmt.Printf("Got data %v, want %v\n", recvB1, Bdata)
+}
+
