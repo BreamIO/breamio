af22dc5a46b39795e227acf8746f5665e1f18620
Zephyyrr <zapmew@gmail.com>
2014-02-28 11:34:07 +0100

    Started testing package. Started with example of usage.

---
+++ gorgonzola/tracker_test.go
@@ -0,0 +1,28 @@
+package gorgonzola_test
+
+import (
+	"fmt"
+	"github.com/maxnordlund/breamio/gorgonzola"
+)
+
+func ExampleUsage() {
+		mock := gorgonzola.GetDriver("mock")
+		if mock == nil {
+			fmt.Println("No Mock driver installed! :(")
+			return
+		}
+        tracker, err := mock.CreateFromId("constant")
+		if err != nil {
+			fmt.Println("No constant \"mocktracker\" implementated! :(")
+			return
+		}
+		
+		tracker.Connect()
+		defer tracker.Close()
+		points, errs := tracker.Stream()
+		select {
+			case p := <-points: fmt.Printf("(%0.2f, %0.2f)\n", p.Filtered.X, p.Filtered.Y)
+			case err = <-errs: fmt.Println("Error:",err)
+		}
+        // Output: (0.50, 0.50)
+}
