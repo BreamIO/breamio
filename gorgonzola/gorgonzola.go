8ad11b054672f0a5ac1562f53e198c8f7ec012ef
Zephyyrr <zapmew@gmail.com>
2014-03-03 15:32:09 +0100

    Added Link function to link evenEmitters and Trackers.

---
+++ gorgonzola/gorgonzola.go
@@ -0,0 +1,27 @@
+package gorgonzola
+
+import (
+	"github.com/maxnordlund/breamio/briee"
+)
+
+func Link(ee briee.EventEmitter, t Tracker) error {
+	publisher := ee.Publish("tracker:etdata", &ETData{}).(chan<- *ETData)
+	dataCh, err = t.Stream()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if r:= recover(); r != nil {
+			println("Caught a runtime panic:", r)
+			//Recover from a close on the publisher channel.
+			//Do not want to bring down entire application
+		}
+	}
+	
+	for data := range dataCh {
+		select {
+			publisher <- data
+			default:
+		}
+	}
+}
