a768a8450d0bf1ae7e28474bedcdcc7024e45044
Zephyyrr <zapmew@gmail.com>
2014-03-03 22:44:50 +0100

    emergency compile fixes and go fmt.

--- gorgonzola/gorgonzola.go
+++ gorgonzola/gorgonzola.go
@@ -7,22 +7,29 @@ import (
 func Link(ee briee.EventEmitter, t Tracker) error {
 	publisher := ee.Publish("tracker:etdata", &ETData{}).(chan<- *ETData)
 	defer close(publisher)
-	dataCh, err = t.Stream()
-	if err != nil {
-		return err
-	}
+	dataCh, errs := t.Stream()
 	defer func() {
-		if r:= recover(); r != nil {
+		if r := recover(); r != nil {
 			println("Caught a runtime panic:", r)
 			//Recover from a close on the publisher channel.
 			//Do not want to bring down entire application
 		}
 	}()
-	
-	for data := range dataCh {
+
+	for {
 		select {
-			case publisher <- data:
-			default: println("[Gorgonzola] Dropped package due to full channel.")
+		case data, ok := <-dataCh:
+			if !ok {
+				break //No more data from tracker. Shutting down.
+			}
+			select {
+			case publisher <- data: // Attempt to send
+			default:
+				println("[Gorgonzola] Dropped package due to full channel.")
+			}
+		case err := <-errs:
+			return err
 		}
 	}
+	return nil
 }
--- gorgonzola/tracker.go
+++ gorgonzola/tracker.go
@@ -24,42 +24,41 @@ func RegisterDriver(typ string, driver Driver) error {
 //Drivers specify tracker operations not connected to any specific tracker and constructors
 //
 //In order to allow multiple different implementations of trackers (Tobii, Mirametrix, Mock, TheEyeTrive) simultaneously, we can not specify all of these in all functions.
-//Instead of hard coding all possible, we can allow injection of new types that follow a common interface. 
+//Instead of hard coding all possible, we can allow injection of new types that follow a common interface.
 //These can then be iterated and processed.
 type Driver interface {
 	//Creates any tracker from this driver.
 	//No promises are made to the uniqueness of the tracker returned.
 	//If no tracker can be returned, a error is returned instead.
 	Create() (Tracker, error)
-	
+
 	//Creates a tracker connected to the identifier string.
 	//The driver is obliged to return that tracker and only that tracker.
 	//If the identifier is invalid or no longer connected, a error is returned.
 	CreateFromId(identifier string) (Tracker, error)
-	
+
 	//Returns a list of valid identifiers that can be used with CreateS.
 	//Empty if no trackers can be created.
 	List() []string
 }
 
-
-//A common interface for all trackers. 
+//A common interface for all trackers.
 type Tracker interface {
 	// Returns a channel of tracking data.
 	// A error channel is also returned.
-	// As the tracker reads data from the device, 
+	// As the tracker reads data from the device,
 	// new ETDatas is created and sent on the channel.
 	// If the channel is full, the tracker discards the data.
 	// If a error occurs while streaming, it is sent along the error channel.
 	Stream() (<-chan *ETData, <-chan error)
-	
+
 	//Initiates a connection between the software driver and the hardware.
 	//Should be called before any other use of the tracker except if method specifies it.
 	Connect() error
-	
+
 	//Closes the tracker connection and performs any other clean up necessary in the driver.
 	io.Closer
-	
+
 	//Optional operation
 	//Calibrates the tracker using the points sent on the channel.
 	//If the channel is buffered, there is no guarantees on when the point is processed by the tracker.
