1452860947599d0d5edf8bf3b2f74069e601041e
Zephyyrr <zapmew@gmail.com>
2014-03-12 15:45:46 +0100

    Reduced main file to only start the logic.

--- breamio.go
+++ breamio.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
-	bl "github.com/maxnordlund/breamio/beenleigh"
+	"github.com/maxnordlund/breamio/aioli"
 	"github.com/maxnordlund/breamio/briee"
-	"github.com/maxnordlund/breamio/gorgonzola"
-	
+	bl "github.com/maxnordlund/breamio/beenleigh"
 )
 
 func main() {
-	logic := bl.New()
-	go etPrinter(logic.RootEmitter())
-	newtrackerEvents := logic.RootEmitter().Publish("new:tracker", bl.TrackerSpec{}).(chan<- bl.TrackerSpec)
-	newtrackerEvents <- bl.TrackerSpec{"mock", "constant", 256}
+	ee := briee.New()
+	io := aioli.New()
+	logic := bl.New(ee, io)
 	logic.ListenAndServe()
-}
-
-func etPrinter(ee briee.EventEmitter) {
-	etEvents := ee.Subscribe("tracker:etdata", &gorgonzola.ETData{}).(<-chan *gorgonzola.ETData)
-	for event := range etEvents {
-		fmt.Println(event)
-	}
 }
