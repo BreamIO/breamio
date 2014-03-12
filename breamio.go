f2c223c7d0ae9a9ba2b29501c45e0f1f6f55445e
Zephyyrr <zapmew@gmail.com>
2014-03-12 15:47:45 +0100

    Apparently, not all changes was added last time. Sorry...

--- breamio.go
+++ breamio.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+	
 	"github.com/maxnordlund/breamio/aioli"
 	"github.com/maxnordlund/breamio/briee"
 	bl "github.com/maxnordlund/breamio/beenleigh"
 )
 
+const (
+	Company = "Bream IO"
+	Product = "Eriver"
+	Version = "v2.0"
+)
+
 func main() {
-	ee := briee.New()
-	io := aioli.New()
-	logic := bl.New(ee, io)
+	fmt.Println("Welcome to", Company, Product, Version)
+	logic := bl.New(briee.New, aioli.New())
 	logic.ListenAndServe()
+	fmt.Println("Thank you for using our product.")
 }
