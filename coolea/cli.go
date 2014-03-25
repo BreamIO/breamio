6ed1b77d812b3278f073411ebd7c035ee231f859
Daniel Langesten <daniel.langest@gmail.com>
2014-03-25 19:33:22 +0000

    Added comments to CLI

--- coolea/cli.go
+++ coolea/cli.go
@@ -16,7 +16,9 @@ func main() {
 	fmt.Print(">")
 	line, isPrefix, err := reader.ReadLine()
 
+	//Read a lot of input
 	for err == nil && !isPrefix {
+		//Parse the last read line
 		parseLine(string(line))
 		fmt.Print(">")
 		line, isPrefix, err = reader.ReadLine()
@@ -27,14 +29,16 @@ func main() {
 	if err != io.EOF {
 		fmt.Println(err)
 	}
-
+	//Done
 	fmt.Printf("Terminated\n")
 }
 
+//Parses the line given
 func parseLine(line string) {
-	//fmt.Println(line)
+	//fmt.Println(line) //Debug
 	commandTokens := strings.Fields(line)
 	ans, parse, tokens := startParse(commandTokens)
+	//Start itterative parsing for all tokens
 	for parse != nil && len(tokens) != 0 {
 		ans, parse, tokens = parse(tokens)
 		if ans != "" {
@@ -43,6 +47,8 @@ func parseLine(line string) {
 	}
 }
 
+//Some kind of parsetree over the available commands
+//--------------------------------------------------
 //Commands
 //List * -- lists * (currently echo *)
 //start
@@ -56,6 +62,7 @@ func parseLine(line string) {
 //set
 //	heatmap id  a=b , c=d ... -- setup heatmap
 //	region id a=b , c=d ... -- setup region
+//--------------------------------------------------
 func startParse(tokens []string) (string, handlerFunc, []string) {
 	if len(tokens) > 1 {
 		switch tokens[0] {
@@ -69,16 +76,18 @@ func startParse(tokens []string) (string, handlerFunc, []string) {
 	return parseError(tokens[0])
 }
 
+//The error given if the parser fails
 func parseError(token string) (string, handlerFunc, []string) {
 	fmt.Println("Error parsing \"" + token + "\".")
 	return "", startParse, nil
 }
 
-// Stuff you can list
+//Parse the subtree of list
 func parseList(tokens []string) (string, handlerFunc, []string) {
 	return tokens[0], startParse, tokens[1:]
 }
 
+//Parse the subtree of set
 func parseSet(tokens []string) (string, handlerFunc, []string) {
 	if len(tokens) > 1 {
 		switch tokens[0] {
@@ -92,6 +101,7 @@ func parseSet(tokens []string) (string, handlerFunc, []string) {
 	return parseError(tokens[0])
 }
 
+//Parse the subtree of heatmap
 func parseHeatMap(tokens []string) (string, handlerFunc, []string) {
 	return parseError(tokens[0])
 	//TODO make update message and go through tokens and update every field in the message
@@ -99,6 +109,7 @@ func parseHeatMap(tokens []string) (string, handlerFunc, []string) {
 	//Also send the message.
 }
 
+//Parse the subtree of region
 func parseRegion(tokens []string) (string, handlerFunc, []string) {
 	return parseError(tokens[0])
 	//TODO make update message, send it, then start from beginning.
