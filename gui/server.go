cb48d8acdfc9b4b14f7b34d34c824d6685714d5a
Max Nordlund <max.nordlund@gmail.com>
2014-03-25 13:46:57 +0100

    Cleaned up loggin and inlined normalize.css

--- gui/server.go
+++ gui/server.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"github.com/maxnordlund/breamio/aioli"
 	"html/template"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,16 +18,22 @@ import (
 type testData struct {
 	EyeTrackers map[int]string
 	logger      *log.Logger
+	Normalize   template.CSS
 }
 
-func newTest() *testData {
+func newTest(pwd string, logger *log.Logger) *testData {
+	normalize, err := ioutil.ReadFile(path.Join(pwd, "normalize.min.css"))
+	if err != nil {
+		logger.Fatalf("Error reading normalize.min.css: %s", err)
+	}
 	return &testData{
 		EyeTrackers: map[int]string{
 			1: "done",
 			2: "",
 			3: "in-progress",
 		},
-		logger: log.New(os.Stdout, "[Server]", log.LstdFlags),
+		logger:    logger,
+		Normalize: template.CSS(string(normalize)),
 	}
 }
 
@@ -45,6 +53,7 @@ var (
 	calibrate = adder{
 		prefix: "calibrate",
 		times:  5,
+		turns:  0,
 		end: func(val *aioli.ExtPkg, ws *websocket.Conn, data *testData) {
 			sec, err := time.ParseDuration("7s")
 			if err != nil {
@@ -62,6 +71,7 @@ var (
 	validate = adder{
 		prefix: "validate",
 		times:  3,
+		turns:  0,
 		end: func(val *aioli.ExtPkg, ws *websocket.Conn, data *testData) {
 			data.logger.Printf("Got final package: %v", val)
 		},
@@ -69,41 +79,49 @@ var (
 )
 
 func (data *testData) handler(ws *websocket.Conn) {
-	go func(ws *websocket.Conn) {
-		for {
-			val := new(aioli.ExtPkg)
-			data.must(websocket.JSON.Receive(ws, val))
-			data.logger.Printf("Event: %v", val)
-			switch val.Event {
-			case "calibrate:start":
-				websocket.JSON.Send(ws, aioli.ExtPkg{
-					Event: "calibrate:next",
-					ID:    val.ID,
-					Data:  val.Data,
-				})
-			case "calibrate:add":
-				calibrate.handle(val, ws, data)
-			case "validate:add":
-				validate.handle(val, ws, data)
-			}
+	for {
+		val := new(aioli.ExtPkg)
+		err := websocket.JSON.Receive(ws, val)
+		if err == io.EOF {
+			data.logger.Println("Got EOF")
+			return
+		} else if err != nil {
+			data.logger.Printf("Couldn't parse val: %s", err)
+			continue
 		}
-	}(ws)
+		data.logger.Printf("Event: %v", val)
+		switch val.Event {
+		case "calibrate:start":
+			websocket.JSON.Send(ws, aioli.ExtPkg{
+				Event: "calibrate:next",
+				ID:    val.ID,
+				Data:  val.Data,
+			})
+		case "calibrate:add":
+			calibrate.handle(val, ws, data)
+		case "validate:add":
+			validate.handle(val, ws, data)
+		}
+	}
 }
 
 type adder struct {
 	prefix string
 	times  int
+	turns  int
 	end    func(val *aioli.ExtPkg, ws *websocket.Conn, data *testData)
 }
 
 func (add *adder) handle(val *aioli.ExtPkg, ws *websocket.Conn, data *testData) {
-	turns := 0
-	data.must(json.Unmarshal(val.Data, &turns))
-	if turns < add.times {
+	// val = make(map[string]interface{})
+	// data.must(json.Unmarshal(val.Data, val))
+	data.logger.Println("WebSocket data: %s", val.Data)
+	if add.turns < add.times {
+		add.turns += 1
 		websocket.JSON.Send(ws, aioli.ExtPkg{
 			Event: add.prefix + ":next",
 			ID:    val.ID,
-			Data:  data.mustMarshal(turns + 1),
+			Data:  val.Data,
 		})
 	} else {
 		websocket.JSON.Send(ws, aioli.ExtPkg{
@@ -118,25 +136,27 @@ func (add *adder) handle(val *aioli.ExtPkg, ws *websocket.Conn, data *testData) 
 }
 
 func main() {
-	data := newTest()
-	wsHandler := websocket.Handler(data.handler)
+	logger := log.New(os.Stdout, "[Server]", log.LstdFlags)
 	pwd, err := os.Getwd()
 	if err != nil {
-		data.logger.Fatalf("Failed to get current working directory: %s\n", err)
+		logger.Fatalf("Failed to get current working directory: %s\n", err)
 	}
+	data := newTest(pwd, logger)
+	wsHandler := websocket.Handler(data.handler)
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
 		// Handle websocket requests separately, but still serve index.html.go
 		if req.Header.Get("Upgrade") == "websocket" && req.Header.Get("Connection") == "Upgrade" {
-			data.logger.Printf("WebSocket Upgrade request for %s\n", req.URL)
+			logger.Printf("WebSocket Upgrade request for %s\n", req.URL)
 			wsHandler.ServeHTTP(rw, req)
 		} else {
-			data.logger.Printf("HTTP %s request for %s\n", req.Method, req.URL)
-			index := template.Must(template.ParseFiles(path.Join(pwd, "index.html.go")))
+			logger.Printf("HTTP %s request for %s\n", req.Method, req.URL)
+			index := template.Must(template.ParseFiles(path.Join(pwd, "index.html")))
 			if err := index.Execute(rw, data); err != nil {
-				data.logger.Fatalf("Template rendering error: %s\n", err)
+				logger.Fatalf("Template rendering error: %s\n", err)
 			}
 		}
 	})
+	logger.Println("Listen on :8080")
 	http.ListenAndServe(":8080", nil)
 }
 
