fc23943dd57552e15d8636db443e200f6ea40aa2
Zephyyrr <zapmew@gmail.com>
2014-03-26 15:02:33 +0100

    big update.

--- gorgonzola/tobii/tobii.go
+++ gorgonzola/tobii/tobii.go
@@ -13,13 +13,13 @@ type GazeDriver struct{}
 
 func (GazeDriver) Create() (Tracker, error) {
 	tracker, err := gaze.AnyEyeTracker()
-	return &GazeTracker{tracker, false}, err
+	return &GazeTracker{tracker, make(chan struct{}), false, 0, 0}, err
 }
 
 func (g GazeDriver) CreateFromId(id string) (Tracker, error) {
 	url := "tet-usb://" + id
 	tracker, err := gaze.EyeTrackerFromURL(url)
-	return &GazeTracker{tracker, false}, err
+	return &GazeTracker{tracker, make(chan struct{}), false, 0, 0}, err
 }
 
 func (GazeDriver) List() (res []string) {
@@ -36,7 +36,10 @@ func (GazeDriver) List() (res []string) {
 
 type GazeTracker struct {
 	*gaze.EyeTracker
+	closer chan struct{}
 	calibrated bool
+	calibrationPoints uint
+	validationPoints uint
 }
 
 func (g GazeTracker) Stream() (<-chan *ETData, <-chan error) {
@@ -53,22 +56,23 @@ func (g GazeTracker) Stream() (<-chan *ETData, <-chan error) {
 
 func (g *GazeTracker) Link(ee briee.PublishSubscriber) {
 	etdataCh := ee.Publish("tracker:etdata", &ETData{}).(chan<- *ETData)
+	//defer close(etdataCh)
+	go g.setupCalibrationEvents(ee)
+	
 	err := g.StartTracking(gobiiOnGazeCallback(etdataCh))
-	errorCh := ee.Publish("tracker:error", err.(error)).(chan<- error)
-	//defer close(errorCh)
-	errorCh <- err
-}
-
-func (g *GazeTracker) Calibrate(points <-chan XYer, errors chan<- error) {
-	for _ = range points {
-		//Mock implementation until Calibration implemented in gobii
-		errors <- NotImplementedError("Calibrate")
+	if err != nil {
+		errorCh := ee.Publish("tracker:error", NewError("")).(chan<- Error)
+		//defer close(errorCh)
+		errorCh <- NewError(err.Error())
 	}
-	g.calibrated = true
 }
 
-func (g GazeTracker) IsCalibrated() bool {
-	return g.calibrated
+func (g *GazeTracker) setupCalibrationEvents(ee briee.PublishSubscriber) {
+	go g.calibrateStartHandler(ee)
+	go g.calibrateAddHandler(ee)
+	
+	go g.validateStartHandler(ee)
+	go g.validateAddHandler(ee)
 }
 
 func (g GazeTracker) String() string {
@@ -86,6 +90,114 @@ func gobiiOnGazeCallback(ch chan<- *ETData) func(data *gaze.GazeData) {
 		etdata.Timestamp = data.Timestamp()
 		//log.Println(etdata)
 		ch <- etdata
+	}
+}
+
+func handleError(errorCh chan<- Error, f func()) func(error) {
+	return func(err error) {
+		if err != nil {
+			errorCh <- NewError(err.Error())
+			return
+		}
+		f()
+	}
+}
+
+func (g *GazeTracker) calibrateStartHandler(ee briee.PublishSubscriber) {
+	inCh := ee.Subscribe("tracker:calibrate:start", struct{}{}).(<-chan struct{})
+	outCh := ee.Publish("tracker:calibrate:next", struct{}{}).(chan<- struct{})
+	errorCh := ee.Publish("tracker:calibrate:error", NewError("")) .(chan<- Error)
+	defer ee.Unsubscribe("tracker:calibrate:start", outCh)
+	//defer close(outCh)
+	
+	for {
+		select {
+			case <- inCh:
+				g.StartCalibration( handleError(errorCh, func() {
+						g.calibrationPoints = 0
+						outCh <- struct{}{}
+					}))
+			case <-g.closer: return
+		}
+	}
+}
+
+func (g *GazeTracker) calibrateAddHandler(ee briee.PublishSubscriber) {
+	inCh := ee.Subscribe("tracker:calibrate:add", Point2D{}).(<-chan Point2D)
+	defer ee.Unsubscribe("tracker:calibrate:add", inCh)
+	
+	nextCh := ee.Publish("tracker:calibrate:next", struct{}{}).(chan<- struct{})
+	//defer close(nextCh)
+	
+	endCh := ee.Publish("tracker:calibrate:end", struct{}{}).(chan<- struct{})
+	//defer close(endCh)
+	
+	vstartCh := ee.Publish("tracker:validate:start", struct{}{}).(chan<- struct{})
+	//defer close(vstartCh)
+	
+	errorCh := ee.Publish("tracker:calibrate:error", NewError("")) .(chan<- Error)
+	//defer close(errorCh)
+	
+	for {
+		select {
+			case p := <- inCh:
+				g.calibrationPoints++
+				if g.calibrationPoints >= 5 {
+					g.StopCalibration( handleError(errorCh, func() {
+							endCh <- struct{}{}
+							vstartCh <- struct{}{}
+						}))
+				} else {
+					g.AddPointToCalibration(gaze.NewPoint2D(p.X(), p.Y()), 
+					handleError(errorCh, func() {
+						nextCh <- struct{}{}
+					}))
+					
+				}
+			case <-g.closer: return
+		}
+	}
+}
+
+func (g *GazeTracker) validateStartHandler(ee briee.PublishSubscriber) {
+	inCh := ee.Subscribe("tracker:validate:start", struct{}{}).(<-chan struct{})
+	nextCh := ee.Publish("tracker:validate:next", struct{}{}).(chan<- struct{})
+	defer ee.Unsubscribe("tracker:validate:start", inCh)
+	//defer close(nextCh)
+	
+	for {
+		select {
+			case <- inCh:
+				g.validationPoints = 0
+				nextCh <- struct{}{}
+			case <-g.closer: return
+		}
+	}
+}
+
+//TODO do actual implementation.
+func (g *GazeTracker) validateAddHandler(ee briee.PublishSubscriber) {
+	inCh := ee.Subscribe("tracker:validate:add", Point2D{}).(<-chan Point2D)
+	defer ee.Unsubscribe("tracker:validate:add", inCh)
+	
+	nextCh := ee.Publish("tracker:validate:next", struct{}{}).(chan<- struct{})
+	//defer close(nextCh)
+	
+	qualityCh := ee.Publish("tracker:validate:end", float64(0)).(chan<- float64)
+	//defer close(qualityCh)
+	
+	for {
+		select {
+			case <- inCh:
+				g.validationPoints++
+				if g.validationPoints >= 5 {
+					//Calculate this using the tobiigaze_get_calibration_point_data_items instead.
+					qualityCh <- float64(0.05)
+				} else {
+					nextCh <- struct{}{}
+				}
+			case <-g.closer: return
+		}
 	}
 }
 
