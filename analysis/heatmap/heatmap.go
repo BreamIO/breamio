55a0cbdf72c5d11294d8b827ac88a60874ccd0ee
Daniel Langesten <daniel.langest@gmail.com>
2014-04-25 21:22:27 +0200

    added default value for color in heatmap

--- analysis/heatmap/heatmap.go
+++ analysis/heatmap/heatmap.go
@@ -79,6 +79,17 @@ func NewGenerator(ee briee.EventEmitter, c *Config) *Generator {
 		publish:           ee.Publish("heatmap:image", new(image.RGBA)).(chan<- *image.RGBA),
 	}
 
+	if (c.color == nil) {
+		g.color = color.RGBA{
+			R: 255,
+			G: 0,
+			B: 0,
+			A: 128,
+		}
+	} else {
+		g.color = c.color
+	}
+
 	go func() {
 		defer ee.Unsubscribe("heatmap:update", updateSettings)
 		for {
