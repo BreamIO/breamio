55bf1023856b9b1ae9b3c4f5d7121ca111b71bdd
Simon Johansson <simj91@gmail.com>
2014-06-09 09:23:13 +0200

    Created first files, not working atm

---
+++ aioli/configLoader/configLoader_test.go
@@ -0,0 +1,12 @@
+package configLoader_test
+
+import (
+	"testing"
+	"log"
+	bl "github.com/maxnordlund/breamio/beenleigh"
+)
+
+func TestConfigLoader(t *testing.T){
+	// Init IOmanager
+	// Init Logic
+}
