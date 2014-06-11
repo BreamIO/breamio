38db84b603dd03e89ce468068a9a64784036ad52
Max Nordlund <max.nordlund@gmail.com>
2014-06-11 10:37:53 +0200

    Update logging

--- aioli/ancientPower/ancient.go
+++ aioli/ancientPower/ancient.go
@@ -18,24 +18,25 @@ func init() {
 	bl.Register(&AncientRun{make(chan struct{})})
 }
 
-type AncientRun struct{
+type AncientRun struct {
 	closing chan struct{}
 }
 
 func (ar *AncientRun) Run(logic bl.Logic) {
-	logger.Println("Initializing AncientPower subsystem.")
+	logger.Println("Initializing AncientPower")
 	ar.closing = make(chan struct{})
 	newCh := logic.RootEmitter().Subscribe("new:ancientpower", bl.Spec{}).(<-chan bl.Spec)
 	defer logic.RootEmitter().Unsubscribe("new:ancientpower", newCh)
-	
+
 	for {
 		select {
-			case event := <-newCh:
-				New(logic, event)
-			case <-ar.closing: return
+		case event := <-newCh:
+			New(logic, event)
+		case <-ar.closing:
+			return
 		}
 	}
-	
+
 }
 
 func (ar *AncientRun) Close() error {
