20d7ea39fe7cfdbbf639f038d728af7703626a54
Simon Johansson <simj91@gmail.com>
2014-07-22 10:48:13 +0200

    Another bug typo

--- analysis/regionStats/generator.go
+++ analysis/regionStats/generator.go
@@ -287,7 +287,7 @@ func inRange(p1, p2 gr.XYer, distance float64) bool {
 // Function used for calculating new fixation points given a new data point
 func newFixation(p1, p2 gr.XYer) *gr.Point2D {
 	dx := p1.X() - p2.X()
-	dy := p2.Y() - p2.Y()
+	dy := p1.Y() - p2.Y()
 
 	return &gr.Point2D {
 		Xf: p1.X() + dx/2,
