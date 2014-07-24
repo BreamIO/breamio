129267edce8416889672659aa46f35b7e17637fa
Johan Fogelström <zapmew@gmail.com>
2014-07-24 23:25:54 +0200

    Reduced duplicate code in the feature support files.

--- support_windows.go
+++ support_windows.go
@@ -3,12 +3,5 @@
 package main
 
 import (
-	_ "github.com/maxnordlund/breamio/gorgonzola/mock"
 	_ "github.com/maxnordlund/breamio/gorgonzola/tobii"
-
-	_ "github.com/maxnordlund/breamio/aioli/access"
-	_ "github.com/maxnordlund/breamio/aioli/ancientPower"
-	_ "github.com/maxnordlund/breamio/webber"
-
-	_ "github.com/maxnordlund/breamio/analysis/regionStats"
 )
