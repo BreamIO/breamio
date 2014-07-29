76813a20e5d7e60f1ac7f4052d767ccd9ace21c8
Daniel Langesten <daniel.langest@gmail.com>
2014-07-29 15:17:59 +0200

    fixed compilation issues

--- mozzarella/mozzarella.go
+++ mozzarella/mozzarella.go
@@ -1,37 +1,42 @@
 package mozzarella
 
-import	(
+import (
+	"github.com/maxnordlund/breamio/briee"
 	"log"
+	"os"
+	//"github.com/maxnordlund/breamio/analysis/regionStats"
 )
 
 type EventAnalyser struct {
-	stopCh	chan struct{}
-	closeCh chan struct{}
-	resetCh chan struct{}
+	stopCh    chan struct{}
+	closeCh   chan struct{}
+	resetCh   chan struct{}
 	publishCh chan struct{}
 }
 
 func NewEventAnalyser() *EventAnalyser {
 	return &EventAnalyser{
-			stopCh: make(chan {}),
-			closeCh: make(chan {}),
-			resetCh: make(chan {}),
-			publishCh: make(chan {}),
-		}
+		stopCh:    make(chan struct{}),
+		closeCh:   make(chan struct{}),
+		resetCh:   make(chan struct{}),
+		publishCh: make(chan struct{}),
+	}
 }
 
 func (e *EventAnalyser) Run() {
-	log := log.New(os.Stderr, "[ EventAnalyser ] ", log.LstdFlags)
+	logger := log.New(os.Stderr, "[ EventAnalyser ] ", log.LstdFlags)
+	logger.Println("Starting")
 }
 
 // Start analysing events in the program
 func (e *EventAnalyser) start(emitters []briee.EventEmitter) {
 	log.Println("Starting analysis of EEs") //TODO might wanna print out wich EEs
-	for _, ee := range emitters {
-		ee.Subscribe("new:regionStats", new(regionStats.Config)).(<-chan *regionStats.Config)
-		defer ee.Unsubscribe("new:regionStats", newChan)
-		//TODO subs and unsubs to ALL messages. =)
-	}
+	//	for _, ee := range emitters {
+	//		ee.Subscribe("new:regionStats", new(regionStats.Config)).(<-chan *regionStats.Config)
+	//defer ee.Unsubscribe("new:regionStats", newChan)
+
+	//TODO subs and unsubs to ALL messages. =)
+	//}
 }
 
 // Stop analysing events in the program
