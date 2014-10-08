9d9a4b0092893647ccf9bbff304ebb124336f215
dwalky <dkarlbom@kth.se>
2014-10-08 13:51:17 +0200

    Added testing for time package

--- time/time_test.go
+++ time/time_test.go
@@ -1,3 +1,23 @@
-	time, err := time.GetGoogleTime()
-	log.Println("time", time, "error", err)
+package time
 
+import (
+	"testing"
+	"time"
+)
+
+func testGetGoogleTime(t *testing.T) {
+	time, err := time.GetGoogleTime()
+	if err != nil {
+		t.Fatal(err, " Crashed")
+	}
+	local := time.Now()
+	var dur time.Duration
+	if local.After(time) {
+		dur = local.Sub(time)
+	} else {
+		dur = time.Sub(time)
+	}
+	if dur.Minutes() > 2.0 { // checking if the clocks are more than 2 minutes apart
+		t.Fatal("time is not synched")
+	}
+}
