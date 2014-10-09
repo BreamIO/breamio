1b44b54845485fabf8265fb6319cc38f6ed25512
Johan Fogelström <zapmew@gmail.com>
2014-10-09 18:47:51 +0200

    Reenabled Tobii for Windows

--- support_windows.go
+++ support_windows.go
@@ -3,6 +3,6 @@
 package main
 
 import (
-	//_ "github.com/maxnordlund/breamio/gorgonzola/tobii"
+	_ "github.com/maxnordlund/breamio/gorgonzola/tobii"
 	_ "github.com/maxnordlund/breamio/hotkeys"
 )
