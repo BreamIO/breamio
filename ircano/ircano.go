bc05c9df9b5c534599b2ef2de2f93ab74a730103
Johan Fogelström <zapmew@gmail.com>
2014-10-17 16:30:12 +0200

    Clean up old experimental code

--- ircano/ircano.go
+++ ircano/ircano.go
@@ -1,39 +1,10 @@
-package main
+package ircano
 
 import (
-	"fmt"
-	ircevent "github.com/thoj/go-ircevent"
-	"time"
+	bl "github.com/maxnordlund/breamio/beenleigh"
+	"github.com/sorcix/irc"
+	"log"
+	"os"
 )
 
-/**
- * TODO:
- * - Add config loader, `config.json`, for username/password
- * - Add callback for commands
- * - Add TCP connection to local breamio server
- * - Add !stats command using the above
- */
-
-func main() {
-	fmt.Println("IRCano", Version, GitSHA)
-	irc := ircevent.IRC("breamio", "breamio")
-	irc.Password = "oauth:rizyskb9a694burwouzh71tsmqxr6lx"
-	irc.AddCallback("001", func(event *ircevent.Event) {
-		fmt.Println(
-			event.Code,
-			event.Raw,
-			event.Nick,
-			event.Host,
-			event.Source,
-			event.User,
-			event.Arguments,
-		)
-	})
-	irc.Connect("irc.twitch.tv:6667")
-	time.Sleep(time.Second * 1)
-	irc.Join("#maxnordlund")
-	irc.Privmsg("#maxnordlund", "Hello World")
-	irc.Privmsg("@maxnordlund", "Hello Private World")
-	time.Sleep(time.Second * 1)
-	irc.Quit()
 }
