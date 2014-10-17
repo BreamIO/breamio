053df33a7d45aa24dab5f6c66d26549f54074a81
Johan Fogelström <zapmew@gmail.com>
2014-10-17 16:30:39 +0200

    Add beenleigh integration

--- ircano/ircano.go
+++ ircano/ircano.go
@@ -7,4 +7,83 @@ import (
 	"os"
 )
 
+const (
+	SettingsEvent = "irc:settings"
+)
+
+const (
+	Username = "breamio"
+	Password = "oauth:rizyskb9a694burwouzh71tsmqxr6lx"
+	Server   = "irc.twitch.tv:6667"
+)
+
+type Settings struct {
+	Username,
+	Password,
+	Server,
+	Channel string
+}
+
+func (s *Settings) FillDefaults() {
+	if s.Username == "" {
+		s.Username = Username
+		if s.Password == "" {
+			s.Password = Password
+		}
+	}
+	if s.Server == "" {
+		s.Server = Server
+	}
+}
+
+type IRCBot struct {
+	settings Settings
+	*irc.Conn
+}
+
+var bot *IRCBot
+var logger *log.Logger
+
+func New(s Settings) *IRCBot {
+	return &IRCBot{s, nil}
+}
+
+func (bot IRCBot) ListenAndServe() {
+
+}
+
+func (bot IRCBot) Close() error {
+	return nil
+}
+
+func init() {
+	logger = log.New(os.Stderr, "[Ircano] ", log.LstdFlags|log.Lshortfile)
+	bot = &IRCBot{}
+	bl.Register(bl.NewRunHandler(func(l bl.Logic, closer <-chan struct{}) {
+		settings := l.RootEmitter().Subscribe(SettingsEvent, Settings{}).(<-chan Settings)
+		defer func() {
+			l.RootEmitter().Unsubscribe(SettingsEvent, settings)
+			err := bot.Close()
+			if err != nil {
+				logger.Println(err)
+			}
+		}()
+
+		for {
+			select {
+			case s, ok := <-settings:
+				if !ok {
+					return
+				}
+				s.FillDefaults()
+				bot.Close()
+				//Skipping error because we do not care since we are creating a new connection soon anyway.
+
+				bot = New(s)
+				go bot.ListenAndServe()
+			case <-closer:
+				return
+			}
+		}
+	}))
 }
