8127cc11949d55dd5ece3dc49936489355cfe0d6
Johan Fogelström <zapmew@gmail.com>
2014-12-08 15:11:24 +0100

    Cleanup and mudularization of Ircano

--- ircano/ircano.go
+++ ircano/ircano.go
@@ -1,13 +1,13 @@
 package ircano
 
 import (
-	"fmt"
 	rs "github.com/maxnordlund/breamio/analysis/regionStats"
 	bl "github.com/maxnordlund/breamio/beenleigh"
 	"github.com/maxnordlund/breamio/briee"
+
 	"github.com/sorcix/irc"
-	"log"
-	"os"
+
+	"fmt"
 	"sort"
 	"strings"
 	"sync"
@@ -34,20 +34,38 @@ type Settings struct {
 	Channel string
 }
 
+type Factory struct{}
+
+func (Factory) String() string {
+	return "Ircano"
+}
+
+func (f Factory) New(c bl.Constructor) bl.Module {
+	bot := &ircBot{
+		SimpleModule: bl.NewSimpleModule(f.String(), c),
+		stats:        make([]string, 0),
+		closer:       make(chan error),
+		messages:     make(chan *irc.Message),
+	}
+
+	go bot.ListenAndServe()
+	return bot
+}
+
 type ircBot struct {
+	bl.SimpleModule
 	settings  Settings
 	stats     []string
 	statsLock sync.RWMutex
 	conn      *irc.Conn
 	closer    chan error
 	messages  chan *irc.Message
+
+	MethodOnRegionStats bl.EventMethod `event:"RegionStats:regions"`
 }
 
 // Implements sort.Interface
 type nameSlice []string
-
-var bot *ircBot
-var logger *log.Logger
 
 func (n nameSlice) Len() int {
 	return len(n)
@@ -59,14 +77,6 @@ func (n nameSlice) Swap(i, j int) {
 
 func (n nameSlice) Less(i, j int) bool {
 	return len(n[i]) < len(n[j])
-}
-
-func New() *ircBot {
-	return &ircBot{
-		stats:    make([]string, 0),
-		closer:   make(chan error),
-		messages: make(chan *irc.Message),
-	}
 }
 
 func (bot *ircBot) FillDefaults() {
@@ -81,18 +91,47 @@ func (bot *ircBot) FillDefaults() {
 	}
 }
 
+func (bot *ircBot) OnRegionStats() {
+	logger.Println("Region stats update")
+
+	names := make(nameSlice, 0, len(regionStats))
+	for name, _ := range regionStats {
+		names = append(names, name)
+	}
+	sort.Sort(names)
+	max := len(names[len(names)-1])
+	stats := []string{
+		"EyeStream statistics:",
+		sprintf("%-*s | Looks/min | Average look time", max, "Region"),
+		strings.Repeat("-", max+32),
+	}
+	for _, name := range names {
+		region := regionStats[name]
+		name = strings.ToTitle(name[:1]) + strings.Replace(name[1:], "-", " ", -1)
+		stats = append(stats, sprintf(
+			"%-*s | %9d | %9.2f seconds",
+			max, name,
+			region.Looks,
+			time.Duration(region.TimeInside).Seconds(),
+		))
+	}
+	bot.statsLock.Lock()
+	defer bot.statsLock.Unlock()
+	bot.stats = stats
+}
+
 func (bot *ircBot) ListenAndServe() error {
 	conn, err := irc.Dial(bot.settings.Server)
 	if err != nil {
 		return err
 	}
-	logger.Println("Connected to", bot.settings.Server)
+	bot.Logger().Println("Connected to", bot.settings.Server)
 	bot.conn = conn
 	go func() {
 		for {
 			msg, err := bot.conn.Decode()
 			if err != nil {
-				logger.Println(err)
+				bot.Logger().Println(err)
 				bot.closer <- err
 				return
 			}
@@ -105,6 +144,10 @@ func (bot *ircBot) ListenAndServe() error {
 	bot.send(irc.NICK, "", bot.settings.Username)
 	bot.send(irc.JOIN, "", bot.settings.Channel)
 
+	return bot.serve()
+}
+
+func (bot *ircBot) serve() error {
 	for {
 		select {
 		case msg := <-bot.messages:
@@ -112,19 +155,12 @@ func (bot *ircBot) ListenAndServe() error {
 			case irc.PING:
 				bot.send(irc.PING, msg.Trailing, msg.Params...)
 			case irc.PRIVMSG:
-				logger.Println("Private message", msg.Trailing)
+				bot.Logger().Println("Private message", msg.Trailing)
 				switch msg.Trailing {
 				case "!Blue circle", "!EyeTracking":
-					bot.send(irc.PRIVMSG, "The blue circle on the screen is a "+
-						"gazemarker. The gazemarker utilizes EyeStream, a software "+
-						"developed by Bream iO, to show my audience where I look on the "+
-						"screen. To present statistics on where I look, simply type "+
-						"\"!ETStats\".", bot.settings.Channel)
+					bot.send(irc.PRIVMSG, infoMessage, bot.settings.Channel)
 				case "!ETStatsInfo":
-					bot.send(irc.PRIVMSG, "Region is the region that the stats is "+
-						"collected for. look/min is how many times I look at the region "+
-						"per minute. average look time is how long I look at the region "+
-						"each time I look there.", bot.settings.Channel)
+					bot.send(irc.PRIVMSG, explanation, bot.settings.Channel)
 				case "!ETStats":
 					go func(bot *ircBot) {
 						bot.statsLock.RLock()
@@ -143,102 +179,29 @@ func (bot *ircBot) ListenAndServe() error {
 }
 
 func (bot *ircBot) send(command, trailing string, params ...string) error {
-	logger.Println("Sending", command, params, trailing)
+	bot.Logger().Println("Sending", command, params, trailing)
 	err := bot.conn.Encode(&irc.Message{
 		Command:  command,
 		Params:   params,
 		Trailing: trailing,
 	})
 	if err != nil {
-		logger.Println(err)
+		bot.Logger().Println(err)
 	}
 	return err
 }
 
 func (bot *ircBot) Close() error {
 	if bot.conn != nil {
-		bot.closer <- nil
-		logger.Println("Quiting IRC")
+		close(bot.closer)
+		bot.Logger().Println("Quiting IRC")
 		return bot.conn.Close()
 	}
 	return nil
 }
 
 func init() {
-	logger = log.New(os.Stderr, "[Ircano] ", log.LstdFlags|log.Lshortfile)
-	bot = New()
-	bl.Register(bl.NewRunHandler(func(l bl.Logic, closer <-chan struct{}) {
-		var regionStatsChan <-chan rs.RegionStatsMap
-		var et briee.EventEmitter
-		go func() {
-			var err error
-			time.Sleep(time.Second * 2)
-			et, err = l.EmitterLookup(1)
-			if err != nil {
-				logger.Println(err)
-				return
-			}
-			regionStatsChan = et.Subscribe(RegionStatsEvent, make(rs.RegionStatsMap)).(<-chan rs.RegionStatsMap)
-		}()
-		settingsChan := l.RootEmitter().Subscribe(SettingsEvent, Settings{}).(<-chan Settings)
-		defer func() {
-			l.RootEmitter().Unsubscribe(SettingsEvent, settingsChan)
-			et.Unsubscribe(RegionStatsEvent, regionStatsChan)
-			err := bot.Close()
-			if err != nil {
-				logger.Println(err)
-			}
-		}()
-
-		for {
-			select {
-			case settings, ok := <-settingsChan:
-				if !ok {
-					return
-				}
-				logger.Println("Settings update")
-				bot.Close()
-				// Skipping error since we are creating a new connection soon anyway.
-
-				bot.settings = settings
-				bot.FillDefaults()
-				go bot.ListenAndServe()
-			case regionStats, ok := <-regionStatsChan:
-				if !ok {
-					return
-				}
-				logger.Println("Region stats update")
-				go func(bot *ircBot, regionStats rs.RegionStatsMap) {
-					names := make(nameSlice, 0, len(regionStats))
-					for name, _ := range regionStats {
-						names = append(names, name)
-					}
-					sort.Sort(names)
-					max := len(names[len(names)-1])
-					stats := []string{
-						"EyeStream statistics:",
-						sprintf("%-*s | Looks/min | Average look time", max, "Region"),
-						strings.Repeat("-", max+32),
-					}
-					for _, name := range names {
-						region := regionStats[name]
-						name = strings.ToTitle(name[:1]) + strings.Replace(name[1:], "-", " ", -1)
-						stats = append(stats, sprintf(
-							"%-*s | %9d | %9.2f seconds",
-							max, name,
-							region.Looks,
-							time.Duration(region.TimeInside).Seconds(),
-						))
-					}
-					bot.statsLock.Lock()
-					defer bot.statsLock.Unlock()
-					bot.stats = stats
-				}(bot, regionStats)
-			case <-closer:
-				return
-			}
-		}
-	}))
+	bl.Register(Factory{})
 }
 
 func sprintf(format string, a ...interface{}) string {
